bda72f9332590a36cca3245ed7d21eef83d78470
Ranesh Rajit <raneshrajit@gmail.com>
2026-06-26 21:10:06 +0530

    feat: add repository for PushToken

---
+++ src/main/java/com/innercircle/repository/PushTokenRepository.java
@@ -0,0 +1,14 @@
+package com.innercircle.repository;
+
+import com.innercircle.model.PushToken;
+import com.innercircle.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PushTokenRepository extends JpaRepository<PushToken, UUID> {
+    List<PushToken> findByUser(User user);
+    Optional<PushToken> findByUserAndPlatform(User user, String platform);
+}
