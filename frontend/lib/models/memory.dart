be8316b3269a6d4104021861b003ddf87d1bbd60
Ranesh Rajit <raneshrajit@gmail.com>
2026-07-01 00:37:55 +0530

    feat(model): add Memory model

---
+++ frontend/lib/models/memory.dart
@@ -0,0 +1,17 @@
+class Memory {
+  final String id;
+  final String fact;
+  final int importance;
+  final DateTime? lastAccessed;
+
+  Memory({required this.id, required this.fact, this.importance = 1, this.lastAccessed});
+
+  factory Memory.fromJson(Map<String, dynamic> json) {
+    return Memory(
+      id: json['id'] ?? '',
+      fact: json['fact'] ?? '',
+      importance: json['importance'] ?? 1,
+      lastAccessed: json['lastAccessed'] != null ? DateTime.parse(json['lastAccessed']) : null,
+    );
+  }
+}
