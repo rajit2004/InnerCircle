5ef2095fa7b3a90bdbd2072074e123ad8c203491
Ranesh Rajit <raneshrajit@gmail.com>
2026-07-27 20:57:23 +0530

    fix: update package name to com.innercircle.frontend

--- frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.frontend
+package com.innercircle.frontend
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
